e6baf89bb57251329968a1a1d648d30209e666a4
guapjorge <alcalajorgea@gmail.com>
2023-09-08 21:48:56 -0700

    Added project

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -25,19 +25,12 @@ const Portfolio = () => {
             demoLink: 'https://mighty-hamlet-98776-5aafaf96d0e0.herokuapp.com/',
             githubLink: 'https://github.com/guapjorge/Note-Saver'
         },
-        // {
-        //     title: 'Tech-blog',
-        //     description: 'bcryptjs, connect-session-sequelize, dotenv, express, express-handlebars, express-session, handlebars, mysql2, sequelize',
-        //     demoLink: 'https://bs3102798-tech-blog-d05f0f76899a.herokuapp.com/',
-        //     githubLink: 'https://github.com/bs3102798/Tech-blog'
-        // },
-
-        // {
-        //     title: 'SQL-Employee-Tracker',
-        //     description: 'dependencies, cfonts, chalk,console.table,inquirer, mysql2',
-        //     demoLink: 'https://project2-demo.com/',
-        //     githubLink: 'https://github.com/bs3102798/SQL-Employee-Tracker'
-        // },
+        {
+            title: 'MoneyMan',
+            description: 'A Budgeting app!',
+            demoLink: 'https://bs3102798.github.io/',
+            githubLink: 'https://github.com/bs3102798/Moneyman'
+        },
         {
             title: 'e-commerce store',
             description: 'A e-commerce website that sells it all!',
